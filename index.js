cf3040ff59f0c46656cea9f7789afa430a7d73db
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-08-16 21:44:55 +0300

    improved StorageProvider api

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 
 const argumented = require('@mstefan99/argumented');
-const StorageProvider = require('./lib/StorageProvider');
+const SecureStorageProvider = require('./lib/SecureStorageProvider');
 
 
 (() => {
-	const s = new StorageProvider;
+	const s = new SecureStorageProvider('a', 'b');
 
-	s.writeFile('./data/text.txt', 'stuff')
-		.then(() => s.readFile('./data/text.txt'))
+	s.save('data/text', (Math.random() * 100000).toString(36))
+		.then(() => s.load('data/text'))
 		.then(data => console.log(data))
 		.catch(err => console.error(err));
 }).call({});
