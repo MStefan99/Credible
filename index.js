62732e0c38fb3f28d90c723a3ab287849a830bd5
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-08-17 14:49:44 +0300

    improved CryptoProvider interface

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const CryptoProvider = require('./lib/CryptoProvider');
 (() => {
 	const s = new CryptoProvider('username', 'password');
 
-	s.pack({customData: 'hello'}, null, false)
-			.then(data => s.unpack(data.encryptedData,data.secureContext))
+	s.pack({customData: 'hello'})
+			.then(data => s.unpack(data))
 			.catch(err => console.error(err))
 			.then(data => console.log(data));
 }).call({});
