34d8b0621908b8ffa26b335a5c72a231b63b75a7
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-08-17 13:11:24 +0300

    reduced hash size

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ const SecureStorageProvider = require('./lib/SecureStorageProvider');
 
 
 (() => {
-	const s = new SecureStorageProvider('a', 'b');
+	const s = new SecureStorageProvider('username', 'password');
 
 	s.save('data/text', {customData: 'hello'}, null, {append: false})
 			.then(context => s.load('data/text', context))
 			.catch(err => console.error(err))
 			.then(data => console.log(data));
 }).call({});
-
