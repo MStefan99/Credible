d5a2c90bf0303b99bf6f0349662e9042c4608e75
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-08-17 18:31:21 +0300

    added user data storage

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ const AccountManager = require('./lib/AccountManager');
 (() => {
 	const accountManager = new AccountManager();
 
-	// accountManager.createUser('mstefan99', 'testtest');
 	accountManager.loadUser('mstefan99', 'testtest')
-			.then(user => user.registerPath('datapath'));
+			// .then(user => user.updatePassword('testtest2'))
+			// .then(user => user.saveData('test', {data: 'string'}))
+			// .then(user => user.loadData('test'))
+			.then(user => user.removeData('test'))
+			.then(data => console.log(data));
 }).call({});
