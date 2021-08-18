c4861f41d99c6c5df5f213a05b9723b596488bbb
Mikhail Stefantsev <mikhail.stefantsev@outlook.com>
2021-08-18 12:23:13 +0300

    added cli and http interface

--- index.js
+++ index.js
@@ -1,31 +1,66 @@
 'use strict';
 
+const http = require('http');
+const https = require('https');
+
 const argumented = require('@mstefan99/argumented');
-const AccountManager = require('./lib/AccountManager');
-const Schema = require('./lib/Schema');
+
+const app = require('./lib/WebInterface');
+const cli = require('./lib/CLIInterface');
 
 
 (() => {
-	const obj = Schema.applySchema({
-		number: 1,
-		secondNumber: 2,
-		array: [1, 2, 3],
-		bool: true,
-		nothing: null,
-		obj: {
-			a: 1,
-			b: '2',
-			c: true,
-			d: [1, 2]
-		}
-	}, {
-		obj: {
-			a: true,
-			d: true
-		},
-		nothing: true,
-		hello: true
-	});
+	argumented.add('username', ['-u', '--username'], ['username'],
+			'Username required for CLI mode');
+	argumented.add('password', ['-p', '--password'], ['password'],
+			'Password required for CLI mode');
+	argumented.add('newPassword', ['-n', '--new-password'], ['newPassword'],
+			'New password to set');
+	argumented.add('key', ['-k', '--key'], ['key'],
+			'Key of the data to save/load');
+	argumented.add('schema', ['-c', '--schema'], ['schema'],
+			'Schema to use for retrieving data');
+	argumented.add('data', ['-d', '--data'], ['data'],
+			'Data to store');
+	argumented.add('server', ['-s', '--server'], null,
+			'Starts a web server to access Credible remotely');
+	argumented.add('ip', ['-i', '--ip'], ['ip'],
+			'An IP address for the web server to listen on. Implies -s.');
+	argumented.add('httpPort', ['--http-port'], ['httpPort'],
+			'A port for HTTP server to listen on. Defaults to 80. Implies -s.');
+	argumented.add('httpsPort', ['--https-port'], ['httpsPort'],
+			'A port for HTTP server to listen on. Defaults to 443. Implies -s.');
+	argumented.add('certFile', ['-e', '--cert-file'], ['certFile'],
+			'Path to HTTPS certificate file');
+	argumented.add('keyFile', ['-y', '--key-file'], ['keyFile'],
+			'Path to HTTPS key file');
+	const config = argumented.parse();
 
-	console.log(obj);
+	if (config.server) {
+		const httpPort = +config.httpPort || 80;
+		http
+				.createServer(app)
+				.listen(httpPort, config.ip || '127.0.0.1');
+	} else {
+		switch (config.positional[0]) {  // Action to perform
+			case 'add-user':
+				cli.addUser(config.username, config.password);
+				break;
+			case 'change-password':
+				cli.changePassword(config.username, config.password, config.newPassword);
+				break;
+			case 'remove-user':
+				cli.removeUser(config.username, config.password);
+				break;
+			case 'read-data':
+				cli.readData(config.username, config.password, config.key, config.schema);
+				break;
+			case 'write-data':
+				cli.writeData(config.username, config.password, config.key, config.data);
+				break;
+			case 'remove-data':
+				cli.removeData(config.username, config.password, config.key);
+				break;
+		}
+	}
 }).call({});
